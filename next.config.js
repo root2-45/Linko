34fffe1e2ce872741c25e3686738d5b0f1f5010b
rutuja <rutujashingote5@gmail.com>
2026-06-21 16:54:53 +0530

    add output:export to generate static files

--- next.config.js
+++ next.config.js
@@ -16,13 +16,39 @@
 // module.exports = nextConfig;
 
 
+
+
+
+
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+// }
+
+// module.exports = nextConfig
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // ← ADD THIS LINE
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,  // ← ADD THIS LINE
   },
 }
 
